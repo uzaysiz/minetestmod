fb91eaf7d2b877bc854fee4046f675cd805690a4
uzayli <uzay@me.me>
2021-08-16 18:51:42 +0300

    ahlagmot

---
+++ init.lua
@@ -0,0 +1,41 @@
+print("ass nbr?")
+
+minetest.register_node("ahlag:yabma", {
+	description = "yabma :/",
+	tiles = {
+		"ahlag_img1.png",
+		"ahlag_img1.png",
+		"ahlag_img1.png",
+		"ahlag_img1.png",
+		"ahlag_img1.png",
+		"ahlag_img1.png"
+	},
+	groups = {cracky = 1}
+})
+
+minetest.register_node("ahlag:limk", {
+	description = "kardesim limk?",
+	tiles = {
+		"ahlag_img2.png",
+		"ahlag_img2.png",
+		"ahlag_img2.png",
+		"ahlag_img2.png",
+		"ahlag_img2.png",
+		"ahlag_img2.png"
+	},
+	groups = {cracky = 1}
+})
+
+minetest.register_node("ahlag:kabo", {
+	description = "kabo diorsa doru dur",
+	tiles = {
+		"ahlag_img3.png",
+		"ahlag_img3.png",
+		"ahlag_img3.png",
+		"ahlag_img3.png",
+		"ahlag_img3.png",
+		"ahlag_img3.png"
+	},
+	groups = {cracky = 1}
+})
+
